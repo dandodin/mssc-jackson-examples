7583718e2d1b8a0dd5c6f94ce2bb34098ac4066e
DodinD1 <62258853+dandodin@users.noreply.github.com>
2020-04-21 10:25:39 -0700

    add deserialize

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -18,7 +18,7 @@ class BeerDtoTest extends BaseTest {
 
     @Test
     void testDeserializeDto() throws IOException {
-        String jsonString = "{\"beerId\":\"857b395b-c57c-4c2d-aa3e-5dd5e281de49\",\"beerName\":\"\",\"beerStyle\":\"\",\"upc\":1223455,\"price\":11.22,\"createdDate\":\"2020-04-21T09:39:06.6104443-07:00\",\"lastUpdatedDate\":\"2020-04-21T09:39:06.6104443-07:00\"}\n";
+        String jsonString = "{\"beerName\":\"Kilt Lifter\",\"beerStyle\":\"Brown\",\"upc\":1223455,\"price\":\"11.22\",\"createdDate\":\"2020-04-21T10:21:-0700\",\"lastUpdatedDate\":\"2020-04-21T10:21:-0700\",\"localDate\":\"20200421\",\"beerId\":\"416c749e-40c6-4e8c-8769-bda24e1d9416\"}\n";
         BeerDto beerDto = objectMapper.readValue(jsonString, BeerDto.class);
         System.out.println(beerDto.toString());
     }
